800d92dbca8a3238898120297141d9d63c9e248e
Yaroslav <yaroslavgolinsiy@gmail.com>
2025-09-17 11:21:00 +0300

    6

--- Aviator_v4/SearchView/FlightDetailView.swift
+++ Aviator_v4/SearchView/FlightDetailView.swift
@@ -224,21 +224,3 @@ struct InfoRow: View {
         .padding(.vertical, 4)
     }
 }
-
-struct SectionHeader: View {
-    let title: String
-    let icon: String
-    
-    var body: some View {
-        HStack(spacing: 8) {
-            Image(systemName: icon)
-                .foregroundColor(.white)
-                .font(.title3)
-            
-            Text(title)
-                .font(.title2)
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-        }
-    }
-}
